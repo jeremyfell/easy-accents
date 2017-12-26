a6ef63758d2176b349aa90ac482095008da33d98
jeremyfell <jeremyafell@gmail.com>
2017-12-26 15:14:45 -0800

    Initial commit

---
+++ content.js
@@ -0,0 +1,150 @@
+matches = {
+"#a'": "á",
+"#e'": "é",
+"#i'": "í",
+"#o'": "ó",
+"#u'": "ú",
+"#y'": "ý",
+"#a`": "à",
+"#e`": "è",
+"#i`": "ì",
+"#o`": "ò",
+"#u`": "ù",
+"#a^": "â",
+"#e^": "ê",
+"#i^": "î",
+"#o^": "ô",
+"#u^": "û",
+"#a:": "ä",
+"#e:": "ë",
+"#i:": "ï",
+"#o:": "ö",
+"#u:": "ü",
+"#y:": "ÿ",
+"#a~": "ã",
+"#n~": "ñ",
+"#o~": "õ",
+"#c,": "ç",
+"#ae": "æ",
+"#oe": "œ",
+
+"#d-": "ð",
+"#a.": "å",
+"#o/": "ø",
+"#th": "þ",
+"#ss": "ß",
+
+"#!": "¡",
+"#?": "¿",
+"#<": "‹",
+"#>": "›",
+"#<<": "«",
+"#>>": "»",
+
+"#.-": "•",
+"#m-": "—",
+"#n-": "–",
+"#.": "°",
+
+"#+-": "±",
+"#!=": "≠",
+"#<=": "≤",
+"#>=": "≥",
+"#/": "÷",
+"#*": "×",
+
+"#^2": "²",
+"#^3": "³",
+"#1/2": "½",
+"#1/4": "¼",
+"#3/4": "¾",
+"#1/3": "⅓",
+"#2/3": "⅔",
+"#1/5": "⅕",
+"#2/5": "⅖",
+"#3/5": "⅗",
+"#4/5": "⅘",
+"#1/6": "⅙",
+"#5/6": "⅚",
+"#1/8": "⅛",
+"#3/8": "⅜",
+"#5/8": "⅝",
+"#7/8": "⅞",
+"#pi": "Π",
+"#mu": "µ",
+
+"#$l": "£",
+"#$e": "€",
+"#$y": "Ұ",
+"#$c": "¢",
+"#$": "",
+"#cmd": "command",
+}
+
+sorted = [];
+
+//Moves object properties to array
+for (m in matches) {
+	sorted.push(m);
+}
+
+//Sorts array by descending order
+sorted.sort(function(a, b){
+  return b.length - a.length;
+});
+
+document.addEventListener("keydown", function(e) {
+	//Gets the html tag of the currently selected element of the page
+	var tag = document.activeElement.tagName;
+	
+	//If Ctrl is pressed, and the selected element is input or textarea
+	if (e.which === 17 && (tag === "INPUT" || tag === "TEXTAREA")) {
+	
+		//Gets text of selected element from start to current cursor position
+		var text = document.activeElement.value.substring(0, document.activeElement.selectionStart);
+		
+		//Gets the position of the last # in the text
+		var pos = text.lastIndexOf("#");
+		
+		//Gets string of text from start to position of #
+		var search = text.substring(pos);
+		
+		for (var s = 0; s < sorted.length; s++) {
+			var match = sorted[s];
+			
+			//If # exists in the text, and search lowercased contains the match at the very start, and the length of search and match are equal
+			if (pos !== -1 && search.toLowerCase().indexOf(match) === 0 && search.length === match.length) {
+			
+				//Gets the string to replace match
+				var replace = matches[match];
+				alert(match);
+				//If the match in search has any uppercase letters, the replace becomes its uppercase counterpart
+				if (isUpper(search.substr(0, match.length))) replace = replace.toUpperCase();
+				
+				//Combines text before #, the new replace string, and text after the end of the # match
+				text = text.substring(0, pos) + replace + document.activeElement.value.substring(pos + match.length);
+				
+				//Sets current selected element's value to text
+				document.activeElement.value = text;
+				
+				//Sets the start and end of cursor selection to right after the new replace string
+				document.activeElement.selectionStart = pos + replace.length;
+				document.activeElement.selectionEnd = pos + replace.length;
+				
+				break;
+			}
+		}
+	}
+});
+
+//Checks each letter of a string, if any are uppercase, returns true
+function isUpper(word) {
+	for (var i = 0; i < word.length; i++) {
+		var c = word[i];
+		
+		if (c.toUpperCase() === c && c.toLowerCase() !== c) {
+			return true;
+		}
+	}
+	return false;
+}
---
+++ popup.js
@@ -0,0 +1,289 @@
+﻿SHORTCUTS = {
+"a'": "á",
+"e'": "é",
+"i'": "í",
+"o'": "ó",
+"u'": "ú",
+"y'": "ý",
+"a`": "à",
+"e`": "è",
+"i`": "ì",
+"o`": "ò",
+"u`": "ù",
+"a^": "â",
+"e^": "ê",
+"i^": "î",
+"o^": "ô",
+"u^": "û",
+"a:": "ä",
+"e:": "ë",
+"i:": "ï",
+"o:": "ö",
+"u:": "ü",
+"y:": "ÿ",
+"a~": "ã",
+"n~": "ñ",
+"o~": "õ",
+"c,": "ç",
+"ae": "æ",
+"oe": "œ",
+
+"d-": "ð",
+"a.": "å",
+"o/": "ø",
+"th": "þ",
+"ss": "ß",
+
+"!": "¡",
+"?": "¿",
+"<": "‹",
+">": "›",
+"<<": "«",
+">>": "»",
+
+".-": "•",
+"m-": "—",
+"n-": "–",
+".": "°",
+
+"+-": "±",
+"!=": "≠",
+"<=": "≤",
+">=": "≥",
+"/": "÷",
+"*": "×",
+
+"^2": "²",
+"^3": "³",
+"1/2": "½",
+"1/4": "¼",
+"3/4": "¾",
+"1/3": "⅓",
+"2/3": "⅔",
+"1/5": "⅕",
+"2/5": "⅖",
+"3/5": "⅗",
+"4/5": "⅘",
+"1/6": "⅙",
+"5/6": "⅚",
+"1/8": "⅛",
+"3/8": "⅜",
+"5/8": "⅝",
+"7/8": "⅞",
+"pi": "Π",
+"mu": "µ",
+
+"$l": "£",
+"$e": "€",
+"$y": "Ұ",
+"$c": "¢",
+"$": "",
+"cmd": "command",
+}
+
+COLORS = {
+	green : "#00A65D"
+}
+
+SOURCE = {
+	add			: "svg/add.svg",
+	arrow		: "svg/arrow.svg",
+	check		: "svg/check.svg",
+	clipboard	: "svg/clipboard.svg",
+	date		: "svg/date.svg",
+	deleting	: "svg/delete.svg",
+	equal		: "svg/equal.svg",
+	key			: "svg/key.svg",
+	manage		: "svg/manage.svg",
+	name		: "svg/name.svg",
+	options		: "svg/options.svg",
+	settings	: "svg/settings.svg",
+	use			: "svg/use.svg"
+}
+
+function clipboardTab() {
+	var clipboardMenuDiv = document.createElement("div");
+	var optionsButton = document.createElement("button");
+	var optionsImage = document.createElement("img");
+	var hashtagP = document.createElement("p");
+	var shortcutInput = document.createElement("input");
+	var clipboardButton = document.createElement("button");
+	var clipboardImage = document.createElement("img");
+	
+	document.documentElement.className = "clipboard-tab";
+	document.body.className = "clipboard-tab";
+	
+	clipboardMenuDiv.id = "clipboard-menu";
+	optionsButton.id = "options-button";
+	optionsImage.id = "options-image";
+	hashtagP.id = "hashtag";
+	shortcutInput.id = "shortcut-input";
+	clipboardButton.id = "clipboard-button";
+	clipboardImage.id = "clipboard-image";
+	
+	optionsButton.className = "copy-button";
+	clipboardButton.className = "copy-button";
+	optionsImage.className = "icon";
+	clipboardImage.className = "icon";
+	hashtagP.className = "hashtag";
+	
+	optionsButton.title = "Edit shortcuts";
+	clipboardButton.title = "Copy shortcut's replacement to clipboard";
+	hashtagP.innerHTML = "#";
+	shortcutInput.setAttribute("type", "text");
+	clipboardButton.disabled = true;
+	
+	clipboardMenuDiv.addEventListener("keydown", function(e) {
+		if (e.which === 17) copyToClipboard();
+	});
+	
+	optionsButton.addEventListener("click", function() {
+		shortcutsTab();
+	});
+	
+	shortcutInput.addEventListener("keypress", function(e) {
+		e.which === 13 ? copyToClipboard() : validShortcut();
+	});
+	
+	shortcutInput.addEventListener("keyup", function() {
+		validShortcut();
+	});
+	
+	clipboardButton.addEventListener("click", function(e) {
+		copyToClipboard();
+	});
+	
+	optionsImage.src = SOURCE.options;
+	clipboardImage.src = SOURCE.clipboard;
+	
+	optionsButton.appendChild(optionsImage);
+	clipboardButton.appendChild(clipboardImage);
+	
+	clipboardMenuDiv.appendChild(optionsButton);
+	clipboardMenuDiv.appendChild(hashtagP);
+	clipboardMenuDiv.appendChild(shortcutInput);
+	clipboardMenuDiv.appendChild(clipboardButton);
+	
+	document.body.appendChild(clipboardMenuDiv);
+	
+	shortcutInput.focus();
+}
+
+
+function shortcutsTab() {
+	var sorted = [];
+	
+	var clipboardMenuDiv = document.getElementById("clipboard-menu");
+	
+	var shortcutsMenuDiv = document.createElement("div");
+	var sortingMenuDiv = document.createElement("div");
+	
+	var shortcutSortButton = document.createElement("button");
+	var replacementSortButton = document.createElement("button");
+	var dateSortButton = document.createElement("button");
+	var usesSortButton = document.createElement("button");
+	
+	var shortcutSortImage = document.createElement("img");
+	var replacementSortImage = document.createElement("img");
+	var dateSortImage = document.createElement("img");
+	var usesSortImage = document.createElement("img");
+	
+	var shortcutsList = document.createElement("list");
+	var shortcutEntryDiv = document.createElement("div");
+	
+	clipboardMenuDiv.parentNode.removeChild(clipboardMenuDiv);
+	
+	shortcutsMenuDiv.id = "shortcuts-menu";
+	sortingMenuDiv.id = "sorting-menu";
+	
+	shortcutSortButton.id = "shortcut-sort-button";
+	replacementSortButton.id = "replacement-sort-button";
+	dateSortButton.id = "date-sort-button";
+	usesSortButton.id = "uses-sort-button";
+	
+	shortcutSortButton.className = "sort-button";
+	replacementSortButton.className = "sort-button";
+	dateSortButton.className = "sort-button";
+	usesSortButton.className = "sort-button";
+	
+	shortcutSortButton.title = "";
+	replacementSortButton.title = "";
+	dateSortButton.title = "";
+	usesSortButton.title = "";
+	
+	shortcutSortImage.className = "icon";
+	replacementSortImage.className = "icon";
+	dateSortImage.className = "icon";
+	usesSortImage.className = "icon";
+	
+	shortcutSortImage.src = SOURCE.key;
+	replacementSortImage.src = SOURCE.name;
+	dateSortImage.src = SOURCE.date;
+	usesSortImage.src = SOURCE.use
+	
+	for (shortcut in SHORTCUTS) {
+		sorted.push(shortcut);
+	}
+	
+	
+	shortcutSortButton.appendChild(shortcutSortImage);
+	replacementSortButton.appendChild(replacementSortImage);
+	dateSortButton.appendChild(dateSortImage);
+	usesSortButton.appendChild(usesSortImage);
+	
+	sortingMenuDiv.appendChild(shortcutSortButton);
+	sortingMenuDiv.appendChild(replacementSortButton);
+	sortingMenuDiv.appendChild(dateSortButton);
+	sortingMenuDiv.appendChild(usesSortButton);
+	
+	
+	shortcutsMenuDiv.appendChild(sortingMenuDiv);
+	shortcutsMenuDiv.appendChild(shortcutsList);
+	shortcutsMenuDiv.appendChild(shortcutEntryDiv);
+	
+	document.body.appendChild(shortcutsMenuDiv);
+	
+}
+
+function validShortcut() {
+	value = document.getElementById("shortcut-input").value;
+	
+	for (shortcut in SHORTCUTS) {
+		if (value === shortcut) {
+		
+			document.getElementById("hashtag").style.borderColor = COLORS.green;
+			document.getElementById("shortcut-input").style.borderColor = COLORS.green;
+			document.getElementById("shortcut-input").title = SHORTCUTS[document.getElementById("shortcut-input").value];
+			document.getElementById("clipboard-button").disabled = false;
+			
+			return;
+		}
+	
+	document.getElementById("hashtag").style.borderColor = null;
+	document.getElementById("shortcut-input").style.borderColor = null;
+	document.getElementById("clipboard-button").disabled = true;
+	document.getElementById("shortcut-input").title = "";
+	
+	}
+}
+
+function copyToClipboard() {
+	if (document.getElementById("clipboard-button").disabled === false) {
+		var temp = document.createElement("textarea");
+		var match = document.getElementById("shortcut-input").value;
+		
+		document.body.appendChild(temp);
+		temp.value = SHORTCUTS[match];
+		
+		temp.focus();
+		temp.select();
+		
+		document.execCommand("Copy");
+		temp.remove();
+		
+		window.close();
+		
+	}
+}
+
+clipboardTab();
